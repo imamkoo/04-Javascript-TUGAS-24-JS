63a299a671b22efcd7f19c91522f7de1a916691e
Imamkoo <imammm.kooo@gmail.com>
2021-12-20 14:12:34 +0700

    Tugas 24 Javascript.js

---
+++ tugas24.js
@@ -0,0 +1,7 @@
+let data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log('Sebelumnya : ' + data);
+asc = data.sort();
+console.log('Ascending : ' + asc);
+dsc = data.reverse();
+console.log('Descending : ' + dsc);
